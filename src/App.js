04de4b56ad116eab7ed4c9c14313c3b57f8c95dd
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-08-13 21:26:36 -0700

    built options prop to add to dropdown component

--- src/App.js
+++ src/App.js
@@ -18,13 +18,28 @@ const items = [
   }
 ];
 
+const options = [
+  {
+    label: 'The Color Red',
+    value: 'red'
+  },
+  {
+    label: 'The Color Green',
+    value: 'green'
+  },
+  {
+    label: 'The Color Blue',
+    value: 'blue'
+  },
+]
+
 const App = () => {
   return (
     <>
     <div>
       {/* <Accordion items={items}/> */}
       {/* <Search /> */}
-      <Dropdown />
+      <Dropdown options={options} />
     </div>
     </>
 
