021d020eddfd83c390cf6da941ce14b85741355e
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-08-16 11:00:10 -0700

    convert component complete

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,12 +1,38 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Convert = ({ language, text }) => {
+  const [translated, setTranslated] = useState('');
+  const [debouncedText, setDebouncedText] = useState(text);
+
+  useEffect(() =>{
+    const timerId = setTimeout(() => {
+      setDebouncedText(text);
+    }, 500);
+
+    return () => {
+      clearTimeout(timerId);
+    }
+  }, [text]);
+
   useEffect(() => {
-    console.log('new language or text');
-  }, [language, text])
+    const doTranslation = async () => {
+      const { data } = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
+        params: {
+          q: debouncedText,
+          target: language.value,
+          key: process.env.REACT_APP_TRANSLATE_API_KEY
+        }
+      });
+      setTranslated(data.data.translations[0].translatedText);
+    };
+    doTranslation();
+  }, [language, debouncedText])
 
   return (
-    <h1>convert</h1>
+    <div>
+      <h1 className="ui header">{translated}</h1>
+    </div>
   );
 }
 
